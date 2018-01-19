ecf6f7d9955a17db71f57f033bf011ae55ccf4e7
Florence <florence@hotmail.fr>
2018-01-19 17:02:55 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
@@ -7,11 +8,14 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { ProfilPage } from '../pages/profil/profil';
+import { TachePage } from '../pages/tache/tache';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { CalendarModule } from 'ionic3-calendar';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +23,9 @@ import { CalendarModule } from 'ionic3-calendar';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    TachePage,
+    ProfilPage
   ],
   imports: [
     CalendarModule,
@@ -32,7 +38,9 @@ import { CalendarModule } from 'ionic3-calendar';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    TachePage,
+    ProfilPage
   ],
   providers: [
     StatusBar,
